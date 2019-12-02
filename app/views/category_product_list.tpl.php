2d7143f9fb21c2fdc9924f554bd1fb94524ce92b
Guillaume <gsylvestre@gmail.com>
2019-12-02 15:55:18 +0100

    fix images links

--- app/views/category_product_list.tpl.php
+++ app/views/category_product_list.tpl.php
@@ -43,7 +43,7 @@
         <div class="product col-xl-3 col-lg-4 col-sm-6">
           <div class="product-image">
             <a href="detail.html" class="product-hover-overlay-link">
-              <img src="<?= $_SERVER["BASE_URI"] ?>/images/produits/1-kiss_tn.jpg" alt="product" class="img-fluid">
+              <img src="<?= $_SERVER["BASE_URI"] ?>/assets/images/produits/1-kiss_tn.jpg" alt="product" class="img-fluid">
             </a>
           </div>
           <div class="product-action-buttons">
@@ -60,7 +60,7 @@
         <div class="product col-xl-3 col-lg-4 col-sm-6">
             <div class="product-image">
               <a href="detail.html" class="product-hover-overlay-link">
-                <img src="<?= $_SERVER["BASE_URI"] ?>/images/produits/2-rose_tn.jpg" alt="product" class="img-fluid">
+                <img src="<?= $_SERVER["BASE_URI"] ?>/assets/images/produits/2-rose_tn.jpg" alt="product" class="img-fluid">
               </a>
             </div>
             <div class="product-action-buttons">
@@ -77,7 +77,7 @@
         <div class="product col-xl-3 col-lg-4 col-sm-6">
             <div class="product-image">
               <a href="detail.html" class="product-hover-overlay-link">
-                <img src="<?= $_SERVER["BASE_URI"] ?>/images/produits/3-panda_tn.jpg" alt="product" class="img-fluid">
+                <img src="<?= $_SERVER["BASE_URI"] ?>/assets/images/produits/3-panda_tn.jpg" alt="product" class="img-fluid">
               </a>
             </div>
             <div class="product-action-buttons">
@@ -94,7 +94,7 @@
         <div class="product col-xl-3 col-lg-4 col-sm-6">
             <div class="product-image">
               <a href="detail.html" class="product-hover-overlay-link">
-                <img src="<?= $_SERVER["BASE_URI"] ?>/images/produits/20-deadpool.jpg" alt="product" class="img-fluid">
+                <img src="<?= $_SERVER["BASE_URI"] ?>/assets/images/produits/20-deadpool.jpg" alt="product" class="img-fluid">
               </a>
             </div>
             <div class="product-action-buttons">
