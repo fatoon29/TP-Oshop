92a32f603f8958a5ff46d651dbe913925c89f2dd
Guillaume <gsylvestre@gmail.com>
2019-12-03 10:22:49 +0100

    create missing models

---
+++ app/Models/Category.php
@@ -0,0 +1,142 @@
+<?php 
+
+class Category 
+{
+
+    private $id;
+    private $name;
+    private $subtitle;
+    private $picture;
+    private $home_order;
+    private $created_at;
+    private $updated_at;
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of subtitle
+     */ 
+    public function getSubtitle()
+    {
+        return $this->subtitle;
+    }
+
+    /**
+     * Set the value of subtitle
+     *
+     * @return  self
+     */ 
+    public function setSubtitle($subtitle)
+    {
+        $this->subtitle = $subtitle;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of picture
+     */ 
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    /**
+     * Set the value of picture
+     *
+     * @return  self
+     */ 
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of home_order
+     */ 
+    public function getHome_order()
+    {
+        return $this->home_order;
+    }
+
+    /**
+     * Set the value of home_order
+     *
+     * @return  self
+     */ 
+    public function setHome_order($home_order)
+    {
+        $this->home_order = $home_order;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     */ 
+    public function getCreated_at()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set the value of created_at
+     *
+     * @return  self
+     */ 
+    public function setCreated_at($created_at)
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updated_at
+     */ 
+    public function getUpdated_at()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set the value of updated_at
+     *
+     * @return  self
+     */ 
+    public function setUpdated_at($updated_at)
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
